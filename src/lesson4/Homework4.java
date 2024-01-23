63c76b09b50d85d71cd55862fb977ed07d5a9477
MukhlisHasanov <156850672+MukhlisHasanov@users.noreply.github.com>
2024-01-23 20:37:29 +0400

    First push

---
+++ src/lesson4/Homework4.java
@@ -0,0 +1,26 @@
+package lesson4;
+
+/** AIT-TR, cohort 42.1, Java Basic, Lesson #4
+ * @Mukhlis_Hasanov
+ * @version 22-Jan-2024
+ */
+
+import java.util.Scanner;
+
+public class Homework4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String a = scanner.next();
+        System.out.println("What is your surname?");
+        String b = scanner.next();
+        System.out.println("Where are you live?");
+        String c = scanner.next();
+        System.out.println("How old are you?");
+        int d = scanner.nextInt(); // We can use String, but I just wanna try int
+        System.out.println("What is your height?");
+        int e = scanner.nextInt();
+        System.out.println("Let's check:");
+        System.out.println("You are " + a + " " + b + ", " + "you are " + d + " years old" + ", " + "you live in " + c + ", " + "and your height is " + e + ", " + "is it correct?");
+    }
+}
