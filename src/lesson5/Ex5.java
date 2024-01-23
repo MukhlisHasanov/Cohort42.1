a0c8065f642d2bbb42730971fc84e64e2ad030c7
MukhlisHasanov <156850672+MukhlisHasanov@users.noreply.github.com>
2024-01-24 01:03:14 +0400

    Lesson5

---
+++ src/lesson5/Ex5.java
@@ -0,0 +1,19 @@
+package lesson5;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Ex #5
+ * @author Mukhlis_Hasanov
+ * @version 23-Jan-2024
+  */
+
+public class Ex5 {
+    public static void main(String[] args) {
+        int t = 30;
+//        if (t > 28) {
+//            System.out.println("It's hot.");
+//        } else {
+//            System.out.println("It's NOT hot.");
+//        }
+        System.out.println(t > 28? "It's hot." : "It's NOT hot.");
+    }
+}
