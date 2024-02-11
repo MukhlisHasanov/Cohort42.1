7de4b80a807c2d03442f01d931e97a2dd87be37c
MukhlisHasanov <156850672+MukhlisHasanov@users.noreply.github.com>
2024-02-11 13:50:46 +0400

    Update Lesson12.java

--- src/lesson12/Lesson12.java
+++ src/lesson12/Lesson12.java
@@ -33,7 +33,7 @@ public class Lesson12 {
         System.out.println("N = " + n);
 }
 
-    static void simpleSort (int[] a) {
+    static void exchangeSort (int[] a) {
         for (int i = 0; i < a.length - 1; i++) {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[i] > a[j]) {
