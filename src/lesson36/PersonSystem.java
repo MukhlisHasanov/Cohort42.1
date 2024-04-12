b0d182893c48e1be0e5939b663ae16fb203525ba
Mukhlis Hasanov <muhlis.gasanov@gmail.com>
2024-04-12 22:32:24 +0400

    les36

--- src/lesson36/PersonSystem.java
+++ src/lesson36/PersonSystem.java
@@ -1,37 +1,44 @@
 package lesson36;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class PersonSystem {
-    static Scanner scanner = new Scanner(System.in);
-    static Map<Integer, Person> idMap = new HashMap<>();
-
     public static void main(String[] args) {
-        List<Person> persons = new ArrayList<>();
-        char operation = 0;
+        int age, id;
+        String name;
+        char operation;
+        Persons persons = new Persons();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Simple personal system: ");
         do {
-            System.out.println("Simple personal system: ");
             System.out.println("Enter operation ([a]dd, [d]elete, [u]pdate, [p]rint, [f]ind, e[x]it: ");
             operation = scanner.next().charAt(0);
             switch (operation) {
                 case 'a':
-                    add(persons);
+                    System.out.print("Add: name & age: ");
+                    name = scanner.next();
+                    age = scanner.nextInt();
+                    persons.add(name, age);
                     break;
                 case 'd':
-                    delete(persons);
+                    System.out.println("Delete: id: ");
+                    id = scanner.nextInt();
+                    persons.delete(id);
                     break;
                 case 'u':
-                    update(persons);
+                    System.out.println("Update: id & name & age:");
+                    id = scanner.nextInt();
+                    name = scanner.next();
+                    age = scanner.nextInt();
+                    persons.update(id, name, age);
                     break;
                 case 'p':
-                    print(persons);
+                    System.out.println(persons);
                     break;
                 case 'f':
-                    find();
+                    System.out.println("Find: id: ");
+                    id = scanner.nextInt();
+                    System.out.println(persons.find(id));
                     break;
                 case 'x':
                     break;
@@ -39,59 +46,5 @@ public class PersonSystem {
                     System.out.println("Undefined operation: " + operation);
             }
         } while (operation != 'x');
-    }
-
-    static void add(List<Person> persons) {
-        System.out.print("Add: name & age: ");
-        String name = scanner.next();
-        int age = scanner.nextInt();
-        Person person = new Person(name, age);
-        idMap.put(person.getId(), person);
-        persons.add(new Person(name, age));
-    }
-
-    static void delete(List<Person> persons) {
-        System.out.println("Delete: id: ");
-        int id = scanner.nextInt();
-        //Person findPerson = findById(persons, id);
-        Person findPerson = idMap.get(id);
-       if (findPerson != null) {
-           persons.remove(findPerson);
-           idMap.remove(id);
-       }
-    }
-
-    static void update(List<Person> persons) {
-        System.out.println("Update: id & name & age:");
-        int id = scanner.nextInt();
-        String name = scanner.next();
-        int age = scanner.nextInt();
-        //Person findPerson = findById(persons, id);
-        Person findPerson = idMap.get(id);
-        if (findPerson != null) {
-            findPerson.setName(name);
-            findPerson.setAge(age);
-        }
-    }
-
-    static void find() {
-        System.out.println("Find: id: ");
-        int id = scanner.nextInt();
-        Person findPerson = idMap.get(id);
-        if (findPerson != null);
-        System.out.println(findPerson);
-    }
-
-    static Person findById(List<Person> persons, int id) {
-        for (Person person : persons) {
-            if (person.getId() == id) {
-                return person;
-            }
-        }
-        return null;
-    }
-
-    static void print(List<Person> persons) {
-        persons.forEach(System.out::println);
     }
 }
